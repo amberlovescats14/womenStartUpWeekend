6d30a4dedc1bbe5ba28e113c683f743099666f20
Amber Jones <amberlovescats14@gmail.com>
2019-09-28 15:47:57 -0500

    amber

--- client/src/components/dashboard/DashboardFunction.js
+++ client/src/components/dashboard/DashboardFunction.js
@@ -7,13 +7,6 @@ const DashboardFunction = () => {
         <Link to="edit-profile" className="btn btn-light">
         <i className="fas fa-user-circle text-primary"></i>
          Edit Profile</Link>
-        <Link to="add-experience" className="btn btn-light">
-        <i class="fas fa-running text-primary"></i> 
-                {' '} Add Experience</Link>
-        <Link to="add-education" className="btn btn-light">
-        <i className="fas fa-users text-primary"></i>
-        {' '}          Add Training Groups
-          </Link>
       </div>
   )
 }
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -44,40 +44,18 @@ router.get('/me', auth, async (req, res) => {
     }
     //pull fields out
     const {
-      company,
-      website,
       location,
       bio,
       status,
-      githubusername,
-      skills,
-      youtube,
-      facebook,
-      twitter,
-      instagram,
-      linkedin
+
     } = req.body;
     //build profile object
 
     const profileFields = {};
     profileFields.user = req.user.id;
-    if (company) profileFields.company = company;
-    if (website) profileFields.website = website;
     if (location) profileFields.location = location;
     if (bio) profileFields.bio = bio;
     if (status) profileFields.status = status;
-    if (githubusername) profileFields.githubusername = githubusername;
-    if(skills) {
-      profileFields.skills = skills.split(',').map(skill => skill.trim())
-    }
-    //build social object
-    profileFields.social = {}
-    if (youtube) profileFields.social.youtube = youtube;
-    if (twitter) profileFields.social.twitter = twitter;
-    if (facebook) profileFields.social.facebook = facebook;
-    if (linkedin) profileFields.social.linkedin = linkedin;
-    if (instagram) profileFields.social.instagram = instagram;
-
     try {
       let profile = await Profile.findOne({user: req.user.id})
       if(profile){
@@ -143,141 +121,4 @@ router.delete('/', auth, async (req, res) => {
     res.status(500).json({msg: `SERVER ERROR`})
   }
 })
-//! @route    PUT api/profile/experience
-//! @desc     add profile experience
-//! @access   Private
-router.put('/experience', [auth,[
-  check('title', `Title is required`).not().isEmpty(),
-  check('from', `From date is required`).not().isEmpty()
-
-
-]], async(req, res)=> {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    return res.status(400).json({errors: errors.array()})
-  }
-  const {title,  location, from , to, current, description} = req.body;
-
-  const newExp = {
-    title,
-    location,
-    from,
-    to,
-    description
-  }
-  try {
-    const profile = await Profile.findOne({user: req.user.id})
-    profile.experience.unshift(newExp);
-    await profile.save()
-
-    res.json(profile)
-  } catch (error) {
-    console.error(error.message)
-    res.status(500).json({msg: `SERVER ERROR`})
-  }
-})
-//! @route    DELETE api/profile/experience/:exp_id
-//! @desc     delete experience from profile
-//! @access   Private
-router.delete('/experience/:exp_id', auth, async(req, res)=> {
-  try {
-    const profile = await Profile.findOne({user: req.user.id})
-    //get removing index
-    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id)
-
-    profile.experience.splice(removeIndex, 1)
-    await profile.save()
-    res.json(profile)
-  } catch (error) {
-    console.error(error.message)
-    res.status(500).json({msg: `SERVER ERROR`})
-  }
-})
-//! @route    PUT api/profile/education
-//! @desc     add profile education
-//! @access   Private
-router.put('/education', [auth,[
-  check('school', `School is required`).not().isEmpty(),
-  check('degree', `Degree is required`).not().isEmpty(),
-  check(`fieldofstudy`, `Field of Study is required`).not().isEmpty()
-
-
-]], async(req, res)=> {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    return res.status(400).json({errors: errors.array()})
-  }
-  const {school, degree, fieldofstudy, from, to, current, description} = req.body;
-
-  const newEdu = {
-    school,
-    degree,
-    fieldofstudy,
-    from,
-    to,
-    current,
-    description
-  }
-  try {
-    const profile = await Profile.findOne({user: req.user.id})
-    profile.education.unshift(newEdu);
-    await profile.save()
-
-    res.json(profile)
-  } catch (error) {
-    console.error(error.message)
-    res.status(500).json({msg: `SERVER ERROR`})
-  }
-})
-//! @route    DELETE api/profile/education/:exp_id
-//! @desc     delete education from profile
-//! @access   Private
-router.delete('/education/:edu_id', auth, async(req, res)=> {
-  try {
-    const profile = await Profile.findOne({user: req.user.id})
-    //get removing index
-    const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id)
-
-    profile.education.splice(removeIndex, 1)
-    await profile.save()
-    res.json(profile)
-  } catch (error) {
-    console.error(error.message)
-    res.status(500).json({msg:`SERVER ERROR`})
-  }
-})
-
-//! @route    GET api/profile/github/:username
-//! @desc     get user repos from github
-//! @access   public
-// @route    GET api/profile/github/:username
-// @desc     Get user repos from Github
-// @access   Public
-router.get('/github/:username', (req, res) => {
-  try {
-    const options = {
-      uri: `https://api.github.com/users/${
-        req.params.username
-      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
-        'githubClientID'
-      )}&client_secret=${config.get('githubSecret')}`,
-      method: 'GET',
-      headers: { 'user-agent': 'node.js' }
-    };
-
-    request(options, (error, response, body) => {
-      if (error) console.error(error);
-
-      if (response.statusCode !== 200) {
-        return res.status(404).json({ msg: 'No Github profile found' });
-      }
-
-      res.json(JSON.parse(body));
-    });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).json({msg: `SERVER ERROR`});
-  }
-});
-
 module.exports = router
